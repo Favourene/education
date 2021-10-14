1a6bad4ffaf361c2f1350ee2a693d8415d147fc5
igbinosa iwinosa <igbinosaiwinosa01@gmail.com>
2021-10-14 20:10:22 +0100

    ish

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 
 // pages
 import Scroll from './pages/Scroll'
